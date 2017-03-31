cdd9bed216024d41a7258975c4a642d3f9cc0077
Dominic Gaiero <dpgaiero@gmail.com>
2017-03-30 18:15:16 -0700

    Added SMS option as well as beautified program.

--- PlusDollarsScraper.py
+++ PlusDollarsScraper.py
@@ -14,6 +14,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import *
 import configparser
+from twilio.rest import TwilioRestClient
 
 # ========================================================
 # Variables
@@ -23,8 +24,53 @@ LOGIN_URL = "https://my.calpoly.edu/cas/login"
 URL = "https://cardcenter.calpoly.edu/student/welcome.php"
 
 # ========================================================
+# Setup session variables
+# ========================================================
+
+
+def configSetup():
+    global USERNAME, PASSWORD, EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_SERVER
+    global EMAIL_PORT, EMAIL_TO, SEND_BY, SEND_METHOD, END_DATE, SMS_ACCOUNT_SID
+    global SMS_AUTH_TOKEN, SMS_SENDING_NUMBER, SMS_RECEIVING_NUMBER
+    fileExists = os.path.isfile("config.ini")
+    config = configparser.ConfigParser()
+    config['CALPOLY_CREDENTIALS'] = {'USERNAME': '', 'PASSWORD': ''}
+    config['EMAIL_SETTINGS'] = {'LOGIN': '', 'PASSWORD': '',
+                                'SERVER': 'smtp.office365.com', 'PORT': '587', 'TO': ''}
+    config['SMS_SETTINGS'] = {
+        'ACCOUNT_SID': '', 'AUTH_TOKEN': '', 'SENDING_NUMBER': '', 'RECEIVING_NUMBER': ''}
+    config['OPTIONS'] = {'SEND_BY': 'EMAIL', 'END': '2017,6,16'}
+    if fileExists:
+        config.read('config.ini')
+        USERNAME = config['CALPOLY_CREDENTIALS']['USERNAME']
+        PASSWORD = config['CALPOLY_CREDENTIALS']['PASSWORD']
+        EMAIL_USERNAME = config['EMAIL_SETTINGS']['LOGIN']
+        EMAIL_PASSWORD = config['EMAIL_SETTINGS']['PASSWORD']
+        EMAIL_SERVER = config['EMAIL_SETTINGS']['SERVER']
+        EMAIL_PORT = int(config['EMAIL_SETTINGS']['PORT'])
+        EMAIL_TO = config['EMAIL_SETTINGS']['TO']
+        SMS_ACCOUNT_SID = config['SMS_SETTINGS']['ACCOUNT_SID']
+        SMS_AUTH_TOKEN = config['SMS_SETTINGS']['AUTH_TOKEN']
+        SMS_SENDING_NUMBER = config['SMS_SETTINGS']['SENDING_NUMBER']
+        SMS_RECEIVING_NUMBER = config['SMS_SETTINGS']['RECEIVING_NUMBER']
+        SEND_BY = config['OPTIONS']['SEND_BY']
+        if SEND_BY == "EMAIL":
+            SEND_METHOD = 1
+        elif SEND_BY == "SMS":
+            SEND_METHOD = 2
+        else:
+            SEND_METHOD = 1
+        END_DATE = config['OPTIONS']['END'].split(",")
+    else:
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+        exit()
+
+# ========================================================
 # Main Program
 # ========================================================
+
+
 def returnBalance():
     session_requests = requests.session()
     result = session_requests.get(LOGIN_URL)
@@ -34,101 +80,99 @@ def returnBalance():
 # ========================================================
 # Dictionary for login
 # ========================================================
+
     form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
     form['username'] = USERNAME
     form['password'] = PASSWORD
     result = session_requests.post(
         LOGIN_URL,
-        data = form,
+        data=form,
         #headers = dict(referer = LOGIN_URL)
-)
+    )
 
 # =========================================================
 # Scrape url
 # =========================================================
+
     result = session_requests.get(URL)
     soup = BeautifulSoup(result.text, "lxml")
     fileName = "temp.webScrape"
-    f = open(fileName,"w+")
+    f = open(fileName, "w+")
     f.write(str(soup))
     f.close()
-    #print (soup.find_all('td'))
     fh = open(fileName)
     balances = []
     for line in fh:
-      if re.search(r"\$.*</td>",line):
-          __inputData = line.rstrip().split("<")
-          __inputData = __inputData[1].split("$")
-          __processedData = __inputData
-          balances.extend(__processedData)
+        if re.search(r"\$.*</td>", line):
+            __inputData = line.rstrip().split("<")
+            __inputData = __inputData[1].split("$")
+            __processedData = __inputData
+            balances.extend(__processedData)
     balance = balances[1]
     fh.close()
     try:
-      os.remove(fileName)
+        os.remove(fileName)
     except OSError:
-      pass
+        pass
     balance = float(balance)
     return balance
 
-'''
-    tree = lxml.html.fromstring(result.content)
-    balance = tree.xpath(r'//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table/tbody/tr[1]/td[2]/text()')
-    result.ok
-    result.status_code
+# ========================================================
+# Send Mail with balance
+# ========================================================
 
-    #print(balance[0])
-'''
 
-def sendMail(amount):
+def sendMail(__message):
     msg = MIMEMultipart()
-    msg['From'] = USERNAME
-    msg['To'] = USERNAME
+    msg['From'] = EMAIL_USERNAME
+    msg['To'] = EMAIL_TO
     msg['Subject'] = "Your Daily Balance"
-
-    body = "Today you have ${} to spend".format(amount)
-    msg.attach(MIMEText(body, 'plain'))
-
-    server = smtplib.SMTP('smtp.office365.com', 587)
+    msg.attach(MIMEText(__message, 'plain'))
+    server = smtplib.SMTP(EMAIL_SERVER, EMAIL_PORT)
     server.starttls()
-    server.login(USERNAME, PASSWORD)
-    text = msg.as_string()
-    server.sendmail(USERNAME, USERNAME, text)
+    server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+    EMAIL_MESSAGE = msg.as_string()
+    server.sendmail(EMAIL_USERNAME, EMAIL_TO, EMAIL_MESSAGE)
     server.quit()
 
-def daysUntil(__year,__month,__day):
-        today = date.today()
-        future = date(__year,__month,__day)
-        diff = future - today
-        return diff.days
+# ========================================================
+# Send SMS With balance
+# ========================================================
 
-def configSetup():
-    fileExists = os.path.isfile("config.ini")
-    config = configparser.ConfigParser()
-    config['CREDENTIALS'] = {'USERNAME': '','PASSWORD': ''}
-    config['END_DATE'] = {'END': '2017,6,16'}
-    if fileExists:
-        config.read('config.ini')
-        username = config['CREDENTIALS']['USERNAME']
-        password = config['CREDENTIALS']['PASSWORD']
-        end_date = config['END_DATE']['END']
-        CONFIGPARAM = [username, password, end_date]
-    else:
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
-        exit()
-    return CONFIGPARAM
+
+def sendSMS(__message):
+
+    client = TwilioRestClient(SMS_ACCOUNT_SID, SMS_AUTH_TOKEN)
+
+    message = client.messages.create(body=__message,
+                                     to=SMS_RECEIVING_NUMBER,
+                                     from_=SMS_SENDING_NUMBER)
+
+# ========================================================
+# Calcualte days remaining until the end of quarter
+# ========================================================
+
+
+def daysUntil(__year, __month, __day):
+    today = date.today()
+    future = date(__year, __month, __day)
+    diff = future - today
+    return diff.days
+
 
 def main():
-    CONFIGPARAM = configSetup()
-    global USERNAME
-    global PASSWORD
-    USERNAME = CONFIGPARAM[0]
-    PASSWORD = CONFIGPARAM[1]
-    END = CONFIGPARAM[2].split(",")
+    configSetup()
     balance = returnBalance()
-    daysLeft = daysUntil(int(END[0]),int(END[1]),int(END[2]))
+    daysLeft = daysUntil(int(END_DATE[0]), int(END_DATE[1]), int(END_DATE[2]))
     amountToday = balance / daysLeft
-    sendMail(round(amountToday, 2))
+    __endDate = "{}/{}/{}".format(END_DATE[0], END_DATE[1], END_DATE[2])
+    __message = "Today you have ${} to spend. \n You have ${} left. \n There are {} days left until the end date ({}).".format(
+        round(amountToday, 2), round(balance, 2), daysLeft, __endDate)
+    if SEND_METHOD == 1:
+        sendMail(__message)
+    elif SEND_METHOD == 2:
+        sendSMS(__message)
+
 
 if __name__ == '__main__':
     main()
